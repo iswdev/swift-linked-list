622f0e5146b72a4bb1f1a5892714aeb67f623b95
Francisco <franciscoigor@gmail.com>
2018-12-04 18:45:36 -0800

    Initial Commit

---
+++ swift-data-structures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-data-structures
+//
+//  Created by User on 2018-12-04.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
